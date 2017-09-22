8e2f3807d911dbbd0869ee4bea9ed14d5d2e4aa4
zxc <zxc@qq.com>
2017-09-22 11:34:16 +0800

    change validateAspect 01

--- sb-web/src/main/java/com/souche/db/aop/ValidateAspect.java
+++ sb-web/src/main/java/com/souche/db/aop/ValidateAspect.java
@@ -3,6 +3,7 @@ package com.souche.db.aop;
 import com.souche.db.annotation.Validate;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -16,19 +17,20 @@ import org.springframework.stereotype.Component;
 public class ValidateAspect {
 
 
-    @Pointcut("@annotation(com.souche.db.annotation.Validate)")// 定义注解类型的切点，只要方法上有该注解，都会匹配
+    @Before("@annotation(validate)")// 定义注解类型的切点，只要方法上有该注解，都会匹配
     public void before(JoinPoint joinPoint, Validate validate) {
-        if (validate.value().equals("zxc")) {
-            String param = (String) joinPoint.getArgs()[joinPoint.getArgs().length - 1];
-            if (param.length() > 30) {
-                try {
-                    System.out.println(param);
-                    System.out.println("ttttt");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        System.out.println("ValidateAspect before method");
+//        if (validate.value().equals("zxc")) {
+//            String param = (String) joinPoint.getArgs()[joinPoint.getArgs().length - 1];
+//            if (param.length() > 30) {
+//                try {
+//                    System.out.println(param);
+//                    System.out.println("ttttt");
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
     }
 
 
