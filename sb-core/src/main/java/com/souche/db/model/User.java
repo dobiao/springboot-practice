a118159f7485faa0031846e5268894a4763ce1dc
wb-wsp312690 <wb-wsp312690@alibaba-inc.com>
2017-09-27 15:48:03 +0800

    使用lombok的getter/setter生成javabean

--- sb-core/src/main/java/com/souche/db/model/User.java
+++ sb-core/src/main/java/com/souche/db/model/User.java
@@ -1,31 +1,12 @@
 package com.souche.db.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
     private String name;
     private Integer age;
     private String password;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
