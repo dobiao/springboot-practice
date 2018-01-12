79c868f701e3d8c13ba7ff3bcbc724456e32a7fb
皮皮今 <506817364@qq.com>
2018-01-12 10:52:15 +0800

    测试Hutool工具类的三个分支

--- sb-web/src/main/java/com/souche/db/hutool/HuToolDemo.java
+++ sb-web/src/main/java/com/souche/db/hutool/HuToolDemo.java
@@ -18,57 +18,63 @@ public class HuToolDemo {
     public static final Log log = LogFactory.get();
 
     public static void main(String[] args) {
-        textDBQuery();
-//        textReadFile();
+        textDBQuery();// 0配置查询数据库,需要指定表名
+        textReadFile();
 //        textJSON_Log();
 //        textStrUtil();
     }
 
     //检测出Hutool的一个Bug,这个地方输出结果还是str,并没有替换成功
-    public static void textStrUtil(){
+    public static void textStrUtil() {
         String str = "{0}bb";
         String chars = "{0}";
         String replacedStr = "aa";
         System.out.println("替换之后的结果为: ");
-        System.out.print(StrUtil.replaceChars(str,chars,replacedStr));
+        System.out.print(StrUtil.replaceChars(str, chars, replacedStr));
     }
-    public static void textReadFile(){
+
+    public static void textReadFile() {
         String address = new ClassPathResource("application.yml").getPath();
         System.out.println(address);
         // 这里不知道为什么没有readUtf8Str方法
         String text = ResourceUtil.readUtf8Str("application.yml");
         System.out.println(text);
     }
-    public static void textJSON_Log(){
-        String json = "[{\"title\":\"小猪\",\"flag\":0,\"orderby\":1,\"type\":1},{\"title\":\"袜子\",\"flag\":0,\"orderby\":2,\"type\":12},{\"title\":\"抱枕\",\"flag\":0,\"orderby\":3,\"type\":123},{\"title\":\"拖鞋\",\"flag\":0,\"orderby\":4,\"type\":1234}]";
 
+    public static void textJSON_Log() {
+        String json = "{\"title\":\"小猪\",\"flag\":0,\"orderby\":1,\"type\":1},{\"title\":\"袜子\",\"flag\":0,\"orderby\":2,\"type\":12},{\"title\":\"抱枕\",\"flag\":0,\"orderby\":3,\"type\":123},{\"title\":\"拖鞋\",\"flag\":0,\"orderby\":4,\"type\":1234}";
         JSONObject obj = null;
         try {
             obj = JSONUtil.parseObj(json);
             System.out.println(obj.toString());
         } catch (Exception e) {
-            log.error("error","JSON字符串格式错误");
+            log.error("error", "JSON字符串格式错误");
             System.out.println(log.getName());
             System.gc();
         }
     }
 
-    public static void textDBQuery(){
+    public static void textDBQuery() {
         try {
             List<Entity> list = SqlRunner.create().findAll("t_cardbins");
-            System.out.println(list);
+            for (Entity entity : list) {
+                System.out.println(entity);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void textDBUpdate(){
-        try {
-            List<Entity> list = SqlRunner.create().findAll("t_cardbins");
-            System.out.println(list);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static void textDBUpdate() {
+//        try {
+//            List<Entity> list = SqlRunner.create().findAll("t_cardbins");
+//            for (Entity entity :
+//                    list) {
+//                System.out.println(entity);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
