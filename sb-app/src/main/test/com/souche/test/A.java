2322123832347c2b94ebe915df3bf03d82d03a2a
dubiao <dubiao@souche.com>
2018-02-02 15:12:12 +0800

    feat:新增一个listener

--- sb-app/src/main/test/com/souche/test/A.java
+++ sb-app/src/main/test/com/souche/test/A.java
@@ -3,6 +3,7 @@ package com.souche.test;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.souche.db.model.User;
+import org.springframework.beans.BeanUtils;
 
 import java.util.List;
 
@@ -14,20 +15,32 @@ public class A {
 
     public static void main(String[] args) {
 
-
         User u1 = new User();
 
+        System.out.println(JSON.toJSONString(u1, WriteNullStringAsEmpty, WriteMapNullValue, PrettyFormat));
+        A a = new A();
+        a.convert(u1);
+        //a.convert2(u1);
+        //a.convert3(u1);
+        System.out.println(JSON.toJSONString(u1, WriteNullStringAsEmpty, WriteMapNullValue, PrettyFormat));
 
+    }
+
+    public void convert(User user) {
         User u2 = new User();
-
         u2.setAge(11);
+        user = u2;
 
-        System.out.println(JSON.toJSONString(u1, WriteNullStringAsEmpty, WriteMapNullValue, PrettyFormat));
-        u1 = u2;
+    }
+    public void convert2(User user) {
+        User u2 = new User();
+        u2.setAge(11);
+        //user = u2;
+        BeanUtils.copyProperties(u2, user);
+    }
 
-        System.out.println(JSON.toJSONString(u1, WriteNullStringAsEmpty, WriteMapNullValue, PrettyFormat));
-
-
+    public void convert3(User user) {
+       user.setAge(11);
     }
 
 
